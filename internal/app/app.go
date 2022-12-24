51ad8bd732fb177a7ef62a5fbb0e911bed1c3c2a
glhfuck <danyacyrex@gmail.com>
2022-12-24 09:43:48 +0300

    add: logrus logger

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,19 +1,18 @@
 package app
 
 import (
-	"log"
-
 	httpControl "github.com/glhfuck/turbo-waffle/internal/controller/http"
 	"github.com/glhfuck/turbo-waffle/internal/infrastructure/repository"
 	"github.com/glhfuck/turbo-waffle/internal/usecase"
 	"github.com/glhfuck/turbo-waffle/pkg/httpserver"
+	"github.com/sirupsen/logrus"
 )
 
 func Run(cfg Config) {
 	db, err := repository.NewPostgresDB(cfg.PostgresConfig)
 
 	if err != nil {
-		log.Fatalf("Can not initialize db: %s", err.Error())
+		logrus.Fatalf("Can not initialize db: %s", err.Error())
 	}
 
 	repository := repository.NewRepository(db)
@@ -23,6 +22,6 @@ func Run(cfg Config) {
 	httpServer := new(httpserver.Server)
 	err = httpServer.Run(cfg.HttpConfig, httpControl.NewRouter(controller))
 	if err != nil {
-		log.Fatalf("Can not run http server: %s", err.Error())
+		logrus.Fatalf("Can not run http server: %s", err.Error())
 	}
 }
